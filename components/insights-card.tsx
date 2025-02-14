cff891ed62081d12a97db61838bd45cee31b9c64
Willbeckh <wileyhndirangu04@gmail.com>
2025-02-14 13:53:31 +0000

    create card ui element

---
+++ components/insights-card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const InsightsCard = () => {
+  return (
+    <Card className="w-full md:w-1/2 lg:w-1/3 xl:w-[200px] cursor-pointer">
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default InsightsCard;
