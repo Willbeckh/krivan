e1c4cce6bf447fd9667e3d2ff23d069d9cff3c18
Willbeckh <wileyhndirangu04@gmail.com>
2025-02-14 20:01:36 +0000

    update card ui display style

--- components/hero.tsx
+++ components/hero.tsx
@@ -57,15 +57,15 @@ export default function Hero() {
             <ArrowUpRight color="black" />
           </motion.button>
 
-          <div className="lg:absolute lg:right-0 lg:top-48 -z-10 p-6">
+          <div className="relative lg:absolute lg:right-0 lg:top-48 -z-10 p-6">
             <div className="absolute inset-10  w-full md:w-1/2 lg:w-1/3 xl:w-[300px] bg-gradient-to-br from-transparent to-[#0A1C26] bg-opacity-50 blur-xl"></div>
             <div className="relative z-10">
               <GrowthChart />
             </div>
           </div>
 
-          <div className="lg:absolute lg:left-0 lg:top-40 -z-10 p-6">
-            <div className="absolute inset-0  w-full md:w-1/2 lg:w-1/3 xl:w-[200px] bg-gradient-to-br from-transparent to-[#0A1C26] bg-opacity-50 blur-xl"></div>
+          <div className="hidden lg:block lg:absolute lg:left-0 lg:top-40 w-full -z-10 p-6">
+            <div className="absolute inset-0 w-full md:w-1/2 lg:w-1/3 xl:w-[200px] bg-gradient-to-br from-transparent to-[#0A1C26] bg-opacity-50 blur-xl"></div>
             <div className="relative z-10">
               <InsightsCard />
             </div>
