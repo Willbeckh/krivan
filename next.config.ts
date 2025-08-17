90d5470c494a3af605a5cece12f39ffa47f9d138
Willbeckh <wileyhndirangu04@gmail.com>
2025-08-17 16:48:42 +0300

    update media remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: `process.env.NEXT_PUBLIC_API_URL?.replace(/^https?:\/\//, "")`,
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
